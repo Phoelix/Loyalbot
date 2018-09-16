c60fcc68e1735659ddb784f10afeab0180f3737a
Phoelix <Lalala007gi!>
2018-09-16 14:40:44 +0300

    Pre Release 0.0.9

--- RU.py
+++ RU.py
@@ -1,5 +1,25 @@
 token = '453318336:AAGjyobZopPBKqlKzokca2KuXfdNjQoLbnQ'
 admins = 'PhoelixFelicitas'
+startbonuses = 3
+
+
+
 welcome = 'Здравствуйте'
 mybal = 'Balance'
 addpin = 'add cup'
+balinfo = 'Balinfo'
+refbut = 'Referal'
+getref = 'Get referal link  https://t.me/myblahtest_bot?start={}'
+
+
+
+"""
+
+welcome -  Приветствие
+mybal   -  Текст кнопки баланса 
+addpin  - 
+balinfo -  Баланс 
+refbut  -  Кнопка получения реферальной ссылки 
+getref  -  Текст реферальной ссылки, должен иметь  https://t.me/название_бота?start={}
+
+"""
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, RegexHandler
 from telegram import ReplyKeyboardMarkup, ReplyMarkup, ReplyKeyboardRemove
 from SQLite import SQLite
 from sqlite3 import Error, IntegrityError
@@ -17,49 +17,84 @@ def start(bot, update):
     user = update.message.from_user
     db = SQLite()
     r_nID = 0
-    for i in range(0, 5):  # (1,4):)
-        r_nID += int(random.randint(1, 9) * 10 ** i)
-    check = db.magic('select nid from memb where nid = (?)', r_nID)
-    try:
-        db.magic(
-            sql='insert or ignore into memb (nid, tgid, fname, uname) VALUES (?,?,?,?)',
-            data=(r_nID,  user.id, user.name, user.first_name))
-    except IntegrityError:
-        r_nID = 0
+    isexistch = db.magic('select tgid from memb where tgid = (?)', (user.id,)).fetchall()
+    if not isexistch:
         for i in range(0, 5):  # (1,4):)
             r_nID += int(random.randint(1, 9) * 10 ** i)
-            try:db.magic(
-                sql='insert or ignore into memb (nid, tgid, fname, uname) VALUES (?,?,?,?)',
-                data=(r_nID, user.id, user.name, user.first_name))
-            except IntegrityError:
-                        user.
-
-    except Error:
-        return  logger.info('User "%s", error "%s"' % (user.id, Error))
-    markup = [[RU.mybal]]
+        check = db.magic('select nid from memb where nid = (?)', (r_nID,)).fetchall()
+        if check:
+            r_nID = 0
+            for i in range(0, 5):  # (1,4):)
+                r_nID += int(random.randint(1, 9) * 10 ** i)
+        try:
+            db.magic(
+                sql='insert into memb (nid, tgid, fname, nname, bal) VALUES (?,?,?,?,?)',
+                data=(r_nID,  user.id, user.first_name, user.name, RU.startbonuses))
+        except Error as e:
+            logger.error('USER Registration ERROR: User: {}\n{}'.format(user.id, e))
+        text = update.message.text.split()
+        if len(text) == 2:
+            db.magic('insert into referals (refer, referal) VALUES (?,?)',
+                data=(text[1], r_nID))
+            logger.info('User {} was invited by {}'.format(user.first_name, text[1]))
+    markup = [[RU.mybal],[RU.refbut]]
     update.message.reply_text(RU.welcome, reply_markup=ReplyKeyboardMarkup(markup), resize_keyboard=True)
 
 
-def addpin(bot, update):                  #TODO split
-    user = update.message.from_user
+def referal(bot, update):
     db = SQLite()
-    try:
-        db.magic(
-            sql='insert or ignore into Memb (tgID, fname, uname) VALUES (?,?,?)',
-            data=(user.id, user.name, user.first_name))
-    except Error:
-        return logger.info('User "%s", error "%s"' % (user.id, Error))
+    user = update.message.from_user
+    refer = db.magic('select nid from memb where tgid = (?)', (user.id,)).fetchall()[0][0]
+    update.message.reply_text(RU.getref.format(refer))
 
 
-def bal(bot, update):
-    user = update.message.from_user
+def addpin(bot, update):
     db = SQLite()
-    try:
-        db.magic(
-            sql='insert or ignore into Memb (tgID, fname, uname) VALUES (?,?,?)',
-            data=(user.id, user.name, user.first_name))
-    except Error:
-        return logger.info('User "%s", error "%s"' % (user.id, Error))
+    user = update.message.from_user
+    bonus_addr = update.message.text.split()
+    adm_list = RU.admins.split()
+
+    if user.username in adm_list or user.name in adm_list:
+        account = db.magic('select bal, fname from memb where nid = {}'.format(str(bonus_addr[0]))).fetchall()
+        bal = int(account[0][0])
+        if len(bonus_addr) == 1:
+            try:
+                bal += 1
+                db.magic('update memb set bal = (?) where nid = {}'.format(str(bonus_addr[0])),
+                         data=(bal,))
+            except Error:
+                return logger.info('User "%s", error "%s"' % (user.id, Error))
+        elif len(bonus_addr) == 2:
+            try:
+                bal += int(bonus_addr[1])
+                db.magic('update memb set bal = (?) where nid = {}'.format(str(bonus_addr[0])),
+                         data=(bal,))
+            except Error:
+                return logger.info('User "%s", error "%s"' % (user.id, Error))
+        return update.message.reply_text('{} {} {}'.format(str(account[0][1]), str(bonus_addr[0]), str(bal)))                                                #TODO text RU.addbonus
+    else:
+        account = db.magic('select bal, fname, nid from memb where tgid = {}'.format(str(user.id))).fetchall()
+        return update.message.reply_text('{} {} {}'.format(str(account[0][1]), str(account[0][2]), str(account[0][0])))     #TODO +RU.balinfo
+
+def release(bot, update):
+    db = SQLite()
+    user = update.message.from_user
+    bonus_addr = update.message.text.split()
+    adm_list = RU.admins.split()
+    if user.username in adm_list or user.name in adm_list:
+        account = db.magic('select bal, fname from memb where nid = {}'.format(str(bonus_addr[1]))).fetchall()
+        count
+
+
+
+
+def balfunc(bot, update):
+    user = update.message.from_user
+    update.message.reply_text(RU.balinfo)
+    db = SQLite()
+    account = db.magic('select bal, fname, nid from memb where tgid = {}'.format(str(user.id))).fetchall()
+    return update.message.reply_text('{} {} {}'.format(str(account[0][1]), str(account[0][2]), str(account[0][0])))
+
 
 def error(bot, update, error):
     logger.info('Update {} caused error {}'.format(update, error), False)
@@ -67,8 +102,10 @@ def error(bot, update, error):
 def main():
     updater = Updater(RU.token)
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(MessageHandler((Filters.text, Filters.user(username=RU.admins)), addpin))
-    updater.dispatcher.add_handler(MessageHandler(RU.mybal, bal))
+    updater.dispatcher.add_handler(RegexHandler('^{}$'.format(RU.mybal), balfunc))
+    updater.dispatcher.add_handler(RegexHandler('^{}$'.format(RU.refbut), referal))
+    updater.dispatcher.add_handler(CommandHandler('use', release))                     #TODO , Filters.user()
+    updater.dispatcher.add_handler(RegexHandler('^\d{5}',  addpin))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_error_handler(error)
     updater.start_polling()
